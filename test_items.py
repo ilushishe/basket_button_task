43374a8a44da9d08969bd9eb873153733c1337d4
ilushishe <ilushishe@gmail.com>
2020-01-21 23:30:48 -0500

    simple refactoring =)

--- test_items.py
+++ test_items.py
@@ -8,9 +8,8 @@ from selenium.common.exceptions import NoSuchElementException
 def test_basket_button_should_be_displayed(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    element_is_displayed = False
     try:
-        element_is_displayed = browser.find_element_by_css_selector(".btn1-add-to-basket").is_displayed()
+        element_is_displayed = browser.find_element_by_css_selector(".btn-add-to-basket").is_displayed()
     except NoSuchElementException:
         element_is_displayed = False
     assert element_is_displayed, "Element isn't displayed or not found"
