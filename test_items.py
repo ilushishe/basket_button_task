f015cc083b785b61c1a2c2094bdd9d80c3148c6f
ilushishe <ilushishe@gmail.com>
2020-01-21 23:26:50 -0500

    some assert fixes

--- test_items.py
+++ test_items.py
@@ -2,10 +2,16 @@ import pytest
 from selenium import webdriver
 import time
 import math
+from selenium.common.exceptions import NoSuchElementException
 
 
 def test_basket_button_should_be_displayed(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    assert browser.find_element_by_css_selector(".btn-add-to-basket").is_displayed(), "Button isn't displayed"
+    element_is_displayed = False
+    try:
+        element_is_displayed = browser.find_element_by_css_selector(".btn1-add-to-basket").is_displayed()
+    except NoSuchElementException:
+        element_is_displayed = False
+    assert element_is_displayed, "Element isn't displayed or not found"
 
